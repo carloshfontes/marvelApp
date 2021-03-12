ae3eeed5c6db5db729c666552fda71b609441cb6
Carlos <carloshenriquenlb@gmail.com>
2021-03-12 11:47:11 -0300

    Setup project

---
+++ MarvelApplication/MarvelApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelApplication
+//
+//  Created by Carlos Fontes on 12/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
