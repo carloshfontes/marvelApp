531d5412689cabf3f9fba040d4e199e187bcfbe2
Carlos <carloshenriquenlb@gmail.com>
2021-03-13 11:42:24 -0300

    Add character worker to help in network calls

---
+++ MarvelApplication/Characters/Scenes/Service/Workers/CharacterWorker.swift
@@ -0,0 +1,45 @@
+//
+//  CharacterWorker.swift
+//  Characters
+//
+//  Created by Carlos Fontes on 13/03/21.
+//
+
+import Foundation
+import Service
+
+typealias CharacterFechCompletion = (Result<[Character], ServiceHandleError>) -> Void
+
+protocol CharacterServiceWorker: class {
+    func getListOfCharactersOrder(by order: CharacterOrderBy, andWithLimit limit: Int, completion: @escaping CharacterFechCompletion)
+}
+
+final class CharactersWorker: CharacterServiceWorker, ServiceClient {
+
+    var session: URLSessionProtocol
+    
+    init(session: URLSessionProtocol = URLSession.shared){
+        self.session = session
+    }
+    
+    
+    func getListOfCharactersOrder(by order: CharacterOrderBy, andWithLimit limit: Int, completion: @escaping (Result<[Character], ServiceHandleError>) -> Void) {
+        
+        guard let request = CharacterProvider.characters(orderBy: order, limit: limit).request else {
+            return completion(.failure(.badRequest))
+        }
+        
+        fetch(withRequest: request, withDecondingType: Marvel.self) { (result) in
+            switch result {
+            
+            case .success(let marvel):
+                completion(.success(marvel.data.results))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    
+    
+}
