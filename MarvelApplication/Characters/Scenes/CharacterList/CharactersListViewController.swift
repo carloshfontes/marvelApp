382fd9786fc1c2c622cac3a6d67bf7cbcabbb9b1
Carlos <carloshenriquenlb@gmail.com>
2021-03-13 10:12:10 -0300

    Added CharactersListViewController

---
+++ MarvelApplication/Characters/Scenes/CharacterList/CharactersListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CharactersListViewController.swift
+//  Characters
+//
+//  Created by Carlos Fontes on 13/03/21.
+//
+
+import Foundation
+
+
+public final class CharactersListViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - View Lifecycle
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Extensions
