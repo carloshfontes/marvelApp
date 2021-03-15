897ade9844567fe82559c8c5fc7c4d84f7952332
Carlos <carloshenriquenlb@gmail.com>
2021-03-15 19:20:16 -0300

    Add MarvelUI target to handle with ui components

---
+++ MarvelApplication/MarvelUI/MarvelUI.h
@@ -0,0 +1,18 @@
+//
+//  MarvelUI.h
+//  MarvelUI
+//
+//  Created by Carlos Fontes on 15/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MarvelUI.
+FOUNDATION_EXPORT double MarvelUIVersionNumber;
+
+//! Project version string for MarvelUI.
+FOUNDATION_EXPORT const unsigned char MarvelUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarvelUI/PublicHeader.h>
+
+
