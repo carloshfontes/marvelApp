0b324f5761cca1f425020e40851d8a249b311049
Carlos <carloshenriquenlb@gmail.com>
2021-03-15 15:28:40 -0300

    Add CharacterListView to work with ui components

---
+++ MarvelApplication/Characters/Scenes/CharacterList/CharacterListView.swift
@@ -0,0 +1,21 @@
+//
+//  CharacterListView.swift
+//  Characters
+//
+//  Created by Carlos Fontes on 15/03/21.
+//
+
+import Foundation
+
+final class CharacterListView: UIView {
+    
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
